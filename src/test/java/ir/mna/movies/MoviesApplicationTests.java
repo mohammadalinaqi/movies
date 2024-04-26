0752ec175568c6d40ee1a308d5301f1b19f200dc
Mohammad Ali N.N <Masterword1994@hotmail.com>
2024-04-26 22:14:15 +0330

    first commit

---
+++ src/test/java/ir/mna/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.mna.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
